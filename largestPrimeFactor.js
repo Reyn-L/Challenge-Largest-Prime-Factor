506a173ed331672b1bc4e3613b60d2f0868645a1
Reyn-L <Reyn.Leong@gmail.com>
2017-06-21 10:19:38 -1000

    Finished

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,37 @@
+/*jshint esversion: 6*/
 exports.largestPrimeFactor = function(n){
-  var primeNumber = 0;
+var primeNumber = 0;
   // do your work here
+  for ( let i = 2; i <= Math.sqrt(n); i++){
+    if ( n % i === 0 && isPrime(i)){
+      primeNumber = i;
 
+    }
+
+  }
   return primeNumber;
 };
+
+function isPrime(n){
+  for( let i = 2; i < Math.sqrt(n); i++){
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+function consoleLogPrimeChecker(n){
+var primeNumber = 0;
+  // do your work here
+  for ( let i = 2; i <= Math.sqrt(n); i++){
+    if ( n % i === 0 && isPrime(i)){
+      primeNumber = i;
+
+    }
+
+  }
+  return primeNumber;
+}
+console.log(isPrime(5));
+console.log(consoleLogPrimeChecker(13195));
+console.log(consoleLogPrimeChecker(600851475143));
