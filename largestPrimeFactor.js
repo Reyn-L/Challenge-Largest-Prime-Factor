1a59e2c2eb76245d770af6ad440da87873a26afe
Reyn-L <Reyn.Leong@gmail.com>
2017-06-21 10:20:50 -1000

    Got rid of consolelogs

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -20,18 +20,4 @@ function isPrime(n){
   }
   return true;
 }
-function consoleLogPrimeChecker(n){
-var primeNumber = 0;
-  // do your work here
-  for ( let i = 2; i <= Math.sqrt(n); i++){
-    if ( n % i === 0 && isPrime(i)){
-      primeNumber = i;
 
-    }
-
-  }
-  return primeNumber;
-}
-console.log(isPrime(5));
-console.log(consoleLogPrimeChecker(13195));
-console.log(consoleLogPrimeChecker(600851475143));
